18ae01a746874f749ea335a17d30ac04bdfb311c
DeekCS <31720001079@std.bau.edu.jo>
2021-11-20 18:32:28 +0200

    Updates

---
+++ precache-manifest.18f581b003814eca35fa3b4b69c63d50.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fde24b4641195e75c86af586c00df7a",
+    "url": "/DeekCS/Modern-UI-UX-in-React/index.html"
+  },
+  {
+    "revision": "42b8a860e6b410a6d1c1",
+    "url": "/DeekCS/Modern-UI-UX-in-React/static/css/main.53d1df3f.chunk.css"
+  },
+  {
+    "revision": "348bf5cbdf8cb5c811d9",
+    "url": "/DeekCS/Modern-UI-UX-in-React/static/js/2.ab3a8a27.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/DeekCS/Modern-UI-UX-in-React/static/js/2.ab3a8a27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42b8a860e6b410a6d1c1",
+    "url": "/DeekCS/Modern-UI-UX-in-React/static/js/main.d6e07f1c.chunk.js"
+  },
+  {
+    "revision": "65a6bd5404b295adc60c",
+    "url": "/DeekCS/Modern-UI-UX-in-React/static/js/runtime-main.7911ecbb.js"
+  },
+  {
+    "revision": "17ade55739aedbbb39e78442cdad6fb3",
+    "url": "/DeekCS/Modern-UI-UX-in-React/static/media/ai.17ade557.png"
+  },
+  {
+    "revision": "fea810ed22f6fc37fab045129a01ed07",
+    "url": "/DeekCS/Modern-UI-UX-in-React/static/media/blog01.fea810ed.png"
+  },
+  {
+    "revision": "e76b2c16b1e4ccf994a24eda75591951",
+    "url": "/DeekCS/Modern-UI-UX-in-React/static/media/blog02.e76b2c16.png"
+  },
+  {
+    "revision": "404d1a405e0cb0936f224fcd374c6cb0",
+    "url": "/DeekCS/Modern-UI-UX-in-React/static/media/blog03.404d1a40.png"
+  },
+  {
+    "revision": "d682b195d844afb2a4ce1e8573602221",
+    "url": "/DeekCS/Modern-UI-UX-in-React/static/media/blog04.d682b195.png"
+  },
+  {
+    "revision": "576efe185f7f571f39d782fde1d28fa9",
+    "url": "/DeekCS/Modern-UI-UX-in-React/static/media/blog05.576efe18.png"
+  },
+  {
+    "revision": "56c4dcf7719731742dfa1db45a776faa",
+    "url": "/DeekCS/Modern-UI-UX-in-React/static/media/logo.56c4dcf7.svg"
+  },
+  {
+    "revision": "3d84899326ac5f0f66149fa037693d18",
+    "url": "/DeekCS/Modern-UI-UX-in-React/static/media/people.3d848993.png"
+  },
+  {
+    "revision": "3c84ae2642b58c3bc28340a7e763fc23",
+    "url": "/DeekCS/Modern-UI-UX-in-React/static/media/possibility.3c84ae26.png"
+  }
+]);
